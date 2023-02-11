642b4996e382243a0e02e308b2030c4c961d0931
sumwai <sumwai@qq.com>
2023-02-11 13:44:05 +0800

    test: compare that with mutex and without mutex

--- lock/main.go
+++ lock/main.go
@@ -1,19 +1,51 @@
 package main
 
 import (
+	"log"
 	"sync"
+	"time"
 )
 
+var c int = 1e8
+
 func main() {
+	WithMutex()
+	WithoutMutex()
+}
+
+func WithMutex() {
 	var count = 0
 	var wg = sync.WaitGroup{}
-	wg.Add(1e6)
-	for i := 0; i < 1e6; i++ {
+	var mutex = sync.Mutex{}
+
+	var start = time.Now()
+	wg.Add(c)
+	for i := 0; i < c; i++ {
+		go func() {
+			mutex.Lock()
+			count++
+			mutex.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	log.Println("with mutex", time.Since(start).Seconds(), "seconds")
+	log.Println("with mutex", count)
+}
+
+func WithoutMutex() {
+	var count = 0
+	var wg = sync.WaitGroup{}
+
+	var start = time.Now()
+	wg.Add(c)
+	for i := 0; i < c; i++ {
 		go func() {
 			count++
 			wg.Done()
 		}()
 	}
 	wg.Wait()
-	println(count)
+	log.Println("without mutex", time.Since(start).Seconds(), "seconds")
+	log.Println("without mutex", count)
 }
